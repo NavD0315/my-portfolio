3c0f4b8d1a62924a625d9f7d4909f59c161e2e6b
Navjot Dhami <dhaminavjot15@gmail.com>
2026-03-25 23:30:32 -0400

    Added JavaScript to toggle project details visibility

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("JavaScript is connected");
+const buttons = document.querySelectorAll(".toggle-btn");
+
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        const details = this.nextElementSibling;
+
+        if (details.style.display === "block") {
+            details.style.display = "none";
+            this.textContent = "Show Details";
+        } else {
+            details.style.display = "block";
+            this.textContent = "Hide Details";
+        }
+    });
+});
